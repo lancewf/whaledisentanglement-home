6b26c20e09cb33d8f400e04a181c9001cfafe5c1
Lance Finfrock <lfinfrock@chef.io>
2018-04-14 16:52:04 -0700

    Adding hatitat
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

---
+++ habitat/README.md
@@ -0,0 +1,9 @@
+# Habitat package: whaledisentanglement-home
+
+## Description
+
+Provide a brief description of the `whaledisentanglement-home` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `whaledisentanglement-home` plan
---
+++ habitat/plan.sh
@@ -0,0 +1,28 @@
+pkg_name=whaledisentanglement_home
+pkg_origin=lancewf
+pkg_version="0.1.0"
+pkg_maintainer="Lance Finfrock <lancewf@gmail.com>"
+pkg_license=("Apache-2.0")
+pkg_source=""
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(core/nginx)
+
+pkg_exports=(
+   [port]=http.listen.port
+   [local_only]=http.listen.local_only
+)
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  mkdir $pkg_prefix/static
+  cp -r src/* $pkg_prefix/static/.
+
+  return 0
+}
+
